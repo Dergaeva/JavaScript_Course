f59d240f559523d787c09c8e4112edf241880104
Elena <dergaeva.elena@mail.ru>
2017-05-14 22:00:37 +0300

    Add files via upload
    first lesson

---
+++ Lessons/1/src/main.js
@@ -0,0 +1,20 @@
+var name = "Елена";
+let privet = "Hello world";
+const PI = 3.14;
+const someDevSalary = NaN;
+
+const user = {
+	name: "Елена",
+	age:"29",
+	salary: "Infinity",
+	dev: true
+}
+
+var Project = {
+	fProject: {
+		status: "easy code student"
+	}
+}
+
+let someFrame = ['Angular','React','Amber'];
+console.log(someFrame);
