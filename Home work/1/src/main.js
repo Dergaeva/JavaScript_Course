072f03abe86bcdf9081f394e7f6ee9d63673c76f
Elena <dergaeva.elena@mail.ru>
2017-05-15 18:55:20 +0300

    first homework

---
+++ Home work/1/src/main.js
@@ -0,0 +1,15 @@
+var contact = {
+	id: 1,
+	name: "Елена",
+	phone: {
+		mainPhone: "0999999999",
+		workPhone: "0500000000"
+	},
+	avatar: "img",
+	gender: "female",
+	age: 29,
+	country: "Украина",
+	knowledge: ['Angular','React'],
+	animals: false
+};
+console.log(contact);
