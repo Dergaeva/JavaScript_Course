221ac5e7a0bbf28152f5cb04907326273e3b4838
Elena <dergaeva.elena@mail.ru>
2017-05-16 21:25:43 +0300

    lesson2

---
+++ 2/js/main.js
@@ -0,0 +1,37 @@
+var user = {
+	cars: ["Santa Fe","Sanata","Acord"],
+	name: "Елена"
+};
+var text = `У пользователя\n ${user.name} \n`
+ + "есть автомобили:";
+
+let myString = 'Елена',
+someService = "youtube",
+name = "Елена";
+var hi = "privet, kak, dela";
+
+
+let qwerty = hi.split(",");
+let qwertys = hi.replace("privet","kak");
+
+const PI=3.14;
+var xxx = "50";
+
+//console.log(50%50);
+
+const year2017 = 2017;
+
+//console.log(year2017%100);//17
+
+//console.log(hi.indexOf('privet'));
+//console.log(hi.includes('privet')); +
+
+
+// console.log(Math.round(3.4));
+// console.log(Math.pow(3,4));
+// console.log(Math.round(Math.random(100)));
+
+
+console.log(Math.min(...[50, 20, 70]));
+
+
