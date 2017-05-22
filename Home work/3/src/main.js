7df0c3e4d2f6202d5eb5630a1a50769ab6f67ed0
Elena <lena_dreames@mail.ru>
2017-05-22 03:13:49 +0300

    version3-1

--- Home work/3/src/main.js
+++ Home work/3/src/main.js
@@ -178,9 +178,9 @@ fizzBuzz(21); // 'Fizz'*/
 // function privet(e) {
 // 	console.log("Вывожу массив :" + e);
 // }
-
 // var myFunct = function (param1, param2, param3, param4) {
 // 	window.arr = [param1, param2, param3];
 // 	return arr;
 // };
+
 // console.log(myFunct(1,2,3, privet(arr)));
