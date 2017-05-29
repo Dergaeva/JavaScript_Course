37b4532a82382fdc57cf00e443862a8c5fb8513f
Dergaeva Elena <dergaeva.elena@mail.ru>
2017-05-29 22:06:23 +0300

    initial project version

---
+++ Lessons/4/js/function.js
@@ -0,0 +1,33 @@
+// function privet() {
+// 	console.log('hello world');
+// }
+
+// privet();
+
+// function sum(num1, num2) {
+// 	var sum = num1 + num2;
+// 	console.log(sum);
+// }
+// sum(10, 20);
+
+
+
+
+// function createFunction = function() {
+// 	return [10, 40, 70]
+// }
+// createFunction();
+
+// var student = function (name) {
+// 	var bigName = name.toUpperCase();
+// 	return console.log(bigName);
+// }
+// student('Елена');
+
+function makeObjnameBig(obj) {
+	var bigName = obj.toUpperCase();
+	return console.log(bigName);
+}
+makeObjnameBig('Елена');
+
+
